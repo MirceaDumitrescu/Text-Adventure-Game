91cce2d3da8a8005684ef67c2f34117ab49475d4
Mircea <mircea@sportify.ro>
2021-08-22 18:05:18 +0300

    Created first method

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-import from methods
+import sys
+import time
+import os
+
+##### Importing method from methods.py (all methods must be stored there or other files, not here)
+from methods import type_sys_text
+
+welcome = """
+
+#####################################################
+       _____  __                __               
+      / ___/ / /_   ____ _ ____/ /____  _      __
+      \__ \ / __ \ / __ `// __  // __ \| | /| / /
+     ___/ // / / // /_/ // /_/ // /_/ /| |/ |/ / 
+    /____//_/ /_/ \__,_/ \__,_/ \____/ |__/|__/  
+
+    __  ___                        ____   ____   ______
+   /  |/  /____   ____   ____     / __ \ / __ \ / ____/
+  / /|_/ // __ \ / __ \ / __ \   / /_/ // /_/ // / __  
+ / /  / // /_/ // /_/ // / / /  / _, _// ____// /_/ /  
+/_/  /_/ \____/ \____//_/ /_/  /_/ |_|/_/     \____/   
+                                                       
+######################################################
+           Copyright Python Task Force 2021
+
+                        > Play <
+                        > Help <
+                        > Quit <
+"""
+
+##### Calling out the function from methods.py to type the welcome message when program run
+type_sys_text(welcome)
--- methods.py
+++ methods.py
@@ -1,1 +1,12 @@
-class Player():
+import sys
+import time
+import os
+
+##### Defined Function to type words slowly
+def type_sys_text(i):
+    for char in i:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.003)
+    player_input = input("> ")
+    os.system("clear")
