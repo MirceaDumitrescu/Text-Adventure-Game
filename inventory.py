8adce1e884dd48386c5b7b6160add33fc39c4fee
Mircea Dumitrescu <mircea@mobipart.ro>
2021-08-24 14:48:29 +0300

    Fixed Inventory
    Fixed inventory to use json file.
    
    Updated the movement function even more. Sucesfully unlocked new map.

--- inventory.py
+++ inventory.py
@@ -1,16 +1,17 @@
+import json
+
 # Create an inventory dictionary with maximum 6 slots
-player_inventory = {
-                "slot1": "0", #for potions only
-                "slot2": "0", #for bullets only
-                "slot3": "0", #for keys only
-                "slot4": "0", #for letters only
-                "slot5": "0", #for clues only
-                "slot6": "0" } #for food only
-                
+with open("data/character.json", "r+") as f:
+    player_inventory = json.load(f)
+
 #method to get item in slot
-def get_inventory(key):
-    return player_inventory[key]
+def get_inventory(job, key):
+    return player_inventory[job][key]
 
 #method to add items to inventory
-def add_inventory(key, value):
-    player_inventory[key] = value
+def set_inventory(job, key, value):
+    player_inventory[job][key] = value
+
+def get_job():
+    res = list(player_inventory.keys())[0]
+    return res
