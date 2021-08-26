792007524216d731ac539e664ac297fdde436050
Mircea Dumitrescu <mircea@mobipart.ro>
2021-08-26 15:00:28 +0300

    preparing code for actions implementation.

--- screens/menu.py
+++ screens/menu.py
@@ -1,18 +1,38 @@
-from methods import type_text
+from methods import type_text, random_examine
 from text_input import text_input
-import screens.directions as m
+import screens.directions as d
 import screens.help as h
 import screens.inventory as i
+from map import zonemap
+from character import myPlayer
+
+
+def examine():
+    examined = zonemap[myPlayer.location]["EXAMINED"]
+    if examined == True:
+        print("There is nothing to examine here. This is your home")
+        game_menu()
+    else:
+        print("There is something to examine")
+        random_examine()
 
 
 def game_menu():
     game_options = f"""
 __________________________________________________________
+|                       GAME MENU                        |
 |                                                        |
 |             What do you want to do next?               |
 |   [m] Change Location | [h] Help Menu | [i] Inventory  |
+|       [b] Go back to Main Menu  | [e] Examine          |
 |________________________________________________________|
     """
     type_text(game_options)
-    menu = {"m": m.get_direction, "h": h.help_game, "i": i.display_inventory}
+    menu = {
+        "m": d.get_direction,
+        "h": h.help_game,
+        "i": i.display_inventory,
+        "b": game_menu,
+        "e": examine,
+    }
     text_input("> ", menu)
